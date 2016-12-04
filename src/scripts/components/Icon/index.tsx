fb778c8f494998014205084fd4c6a59be48297c9
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2016-12-04 22:57:39 +0300

    Ignore styles in test env

--- src/scripts/components/Icon/index.tsx
+++ src/scripts/components/Icon/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
+import * as styles from './styles.css';
 
 export default class Icon extends React.Component<any, any> {
   render() {
     return (
-      <div>sup</div>
+      <div className={styles.icon}>sup</div>
     );
   }
 }
